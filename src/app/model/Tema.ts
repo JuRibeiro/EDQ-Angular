a3be96e5c86bd6d651ea1e06633c9c2861eba05b
Matheus094-tech <80859363+Matheus094-tech@users.noreply.github.com>
2021-07-01 10:19:43 -0300

    Models alimentadas, tipoUsuario, foto

---
+++ src/app/model/Tema.ts
@@ -0,0 +1,10 @@
+import { Postagem } from "./Postagem"
+
+export class Tema{
+    public id: number
+    public materia: string
+    public anoConteudo: string
+    public descricao: string
+    public postagem: Postagem[]
+
+}

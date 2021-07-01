fcc4fa83fd4d43a46654d086ebb7d95bd344b53a
Matheus094-tech <80859363+Matheus094-tech@users.noreply.github.com>
2021-07-01 09:38:17 -0300

    Models alimentadas Matheus

---
+++ src/app/model/UserLogin.ts
@@ -0,0 +1,9 @@
+export class UserLogin{
+    public token: string
+    public id: number   
+    public nomeCompleto: string
+    public email: string
+    public senha: string
+    public foto: string
+    public tipo: string
+}

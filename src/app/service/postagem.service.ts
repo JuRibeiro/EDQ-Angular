28d5c313b69f772c3eb53947da4c220f3467da68
JuRibeiro <ju.ribeiro1208@gmail.com>
2021-07-12 15:09:36 -0300

    Ajustes imagem, letras e afins

--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -41,7 +41,6 @@ export class PostagemService {
     return this.http.get<Postagem[]>(`${environment.uri}/postagens/titulo/${titulo}`, this.token)
   }
 
-
   postPostagem(postagem:Postagem):Observable<Postagem>{
     return this.http.post<Postagem>(`${environment.uri}/postagens`, postagem, this.token)
   }
